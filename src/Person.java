17508bf50f27a6d0c0daf25abb5ff04e46a68614
Moh. Sirajul Umam <sirajul.umam8@gmail.com>
2024-06-08 15:32:28 +0700

    Constructor Overloading

--- src/Person.java
+++ src/Person.java
@@ -8,6 +8,14 @@ public class Person {
         address = paramAddress;
     }
 
+    Person(String paramName) {
+        this(paramName, null);
+    }
+
+    Person() {
+        this(null);
+    }
+
     void sayHello(String paramName) {
         System.out.println("Hello " + paramName + ", My Name is " + name);
     }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -8,10 +8,14 @@ public class PersonApp {
 
         person.sayHello("Budi");
 
-        Person person2 = new Person("Budi", "Jakarta");
+        Person person2 = new Person("Budi");
+
         Person person3;
 
-        person3 = new Person("Joko", "Cirebon");
+        person3 = new Person();
+
+        person3.name = "Mustafa";
+        person3.address = "Jakarta";
 
         person3.sayHello("Ahmad");
 
