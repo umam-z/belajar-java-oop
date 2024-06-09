97508e0a99632ca42b0f609a531c2e9fe3e52885
Moh. Sirajul Umam <sirajul.umam8@gmail.com>
2024-06-09 12:43:03 +0700

    Method Overriding

--- src/Manager.java
+++ src/Manager.java
@@ -2,6 +2,6 @@ public class Manager {
     String name;
 
     void sayHello(String name) {
-        System.out.println("Hi " + name + ", My Name is " + this.name);
+        System.out.println("Hi " + name + ", My Name is Manager " + this.name);
     }
 }
--- src/VicePresident.java
+++ src/VicePresident.java
@@ -1,3 +1,5 @@
 public class VicePresident extends Manager {
-
+    void sayHello(String name) {
+        System.out.println("Hi " + name + ", My Name is VP " + this.name);
+    }
 }
