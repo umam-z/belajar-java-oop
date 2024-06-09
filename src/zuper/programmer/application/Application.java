f18f26f6b13b94eee82df5b9c5aa2bc32f6caec2
Moh. Sirajul Umam <sirajul.umam8@gmail.com>
2024-06-09 21:37:58 +0700

    Import

--- src/zuper/programmer/application/Application.java
+++ src/zuper/programmer/application/Application.java
@@ -1,11 +1,13 @@
 package zuper.programmer.application;
 
-import zuper.programmer.data.Product;
+import zuper.programmer.data.*;
 
 public class Application {
     public static void main(String[] args) {
         Product product = new Product("Toshiba L840 Core I3", 500000);
         System.out.println(product.name);
         System.out.println(product.price);
+
+        Data data = new Data();
     }
 }
---
+++ src/zuper/programmer/data/Data.java
@@ -0,0 +1,5 @@
+package zuper.programmer.data;
+
+public class Data {
+
+}
