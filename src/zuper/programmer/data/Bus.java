6d5ef1dcc7555ac6e411d245efa762cc82153563
Moh. Sirajul Umam <sirajul.umam8@gmail.com>
2024-06-09 23:14:07 +0700

    Default Method

---
+++ src/zuper/programmer/data/Bus.java
@@ -0,0 +1,24 @@
+package zuper.programmer.data;
+
+public class Bus implements Car {
+
+    public void drive() {
+        System.out.println("Bus Drive");
+    }
+
+    public int getTire() {
+        return 8;
+    }
+
+    public String getBrand() {
+        return "HINO";
+    }
+
+    public boolean isMaintenance() {
+        return false;
+    }
+
+    public boolean isBig() {
+        return true;
+    }
+}
--- src/zuper/programmer/data/Car.java
+++ src/zuper/programmer/data/Car.java
@@ -3,4 +3,8 @@ package zuper.programmer.data;
 public interface Car extends HasBrand, IsMaintenance {
     void drive();
     int getTire();
+
+    default boolean isBig() {
+        return false;
+    }
 }
