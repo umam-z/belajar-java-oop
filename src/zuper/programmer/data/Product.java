5c64e0b8cb706ea3c9bce8612f85b23a8508fc8e
Moh. Sirajul Umam <sirajul.umam8@gmail.com>
2024-06-09 21:02:46 +0700

    Package

---
+++ src/zuper/programmer/data/Product.java
@@ -0,0 +1,11 @@
+package zuper.programmer.data;
+
+public class Product {
+    String name;
+    int price;
+    
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
