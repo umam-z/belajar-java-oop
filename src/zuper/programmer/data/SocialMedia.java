71a33d84659b209679e74b39dfe7ec80f3e37576
Moh. Sirajul Umam <sirajul.umam6@gmail.com>
2024-06-13 10:32:12 +0700

    Final Method

--- src/zuper/programmer/data/SocialMedia.java
+++ src/zuper/programmer/data/SocialMedia.java
@@ -4,11 +4,15 @@ class SocialMedia {
 
 }
 
-final class Facebook extends SocialMedia{
-   
-}
-
-// Eror
-// class FakeFacebook extends Facebook {
+class Facebook extends SocialMedia{
+   final void login(String username, String password) {
     
-// }
+   }
+}
+   
+class FakeFacebook extends Facebook {
+    // error
+    // void login(String username, String password) {
+ 
+    // }
+}
