919f434f304b0f6c18713f8ca3aae84804aa98aa
Moh. Sirajul Umam <sirajul.umam6@gmail.com>
2024-06-13 15:03:49 +0700

    Record Class

---
+++ src/zuper/programmer/data/LoginRequest.java
@@ -0,0 +1,15 @@
+package zuper.programmer.data;
+
+public record LoginRequest(String username, String password) {
+    public LoginRequest {
+        System.out.println("Membuat object LoginRequest");
+    }
+
+    public LoginRequest (String username) {
+        this(username, "");
+    }
+
+    public LoginRequest() {
+        this("","");
+    }
+}
