17bb534958f527296e0f54db4857786a39b18cff
Moh. Sirajul Umam <sirajul.umam6@gmail.com>
2024-06-13 18:18:51 +0700

    Exception

---
+++ src/zuper/programmer/application/ValidationApp.java
@@ -0,0 +1,19 @@
+package zuper.programmer.application;
+
+import zuper.programmer.data.LoginRequest;
+import zuper.programmer.error.ValidationException;
+import zuper.programmer.util.ValidationUtil;
+
+public class ValidationApp {
+    public static void main(String[] args) {
+        LoginRequest loginRequest = new LoginRequest("Umam", "Rahasia");
+        try {
+            ValidationUtil.validate(loginRequest);
+            System.out.println("Data Valid");
+        } catch (ValidationException | NullPointerException e) {
+            System.out.println("Data tidak valid : " + e.getMessage());
+        } finally {
+            System.out.println("Selalu akan dieksekusi");
+        }
+    }
+}
---
+++ src/zuper/programmer/error/ValidationException.java
@@ -0,0 +1,7 @@
+package zuper.programmer.error;
+
+public class ValidationException extends Throwable{
+    public ValidationException(String message) {
+        super(message);
+    }
+}
